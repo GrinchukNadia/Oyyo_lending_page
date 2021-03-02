79f9f0ca02c23c0b3b9dc157e9d96e80bf4d7c43
Надежда <grennad991@gmail.com>
2021-03-02 22:25:42 +0200

    Made adaptive for tablet did corrections for all adaptive

--- #src/js/script.js
+++ #src/js/script.js
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     // конец спойлер, аккардеон
 
+
     // кнопка прокрутки сайта наверх
     const scrollTopBtn = document.querySelector('.scroll_top');
 
@@ -32,6 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
         window.scrollTo(0, 0);
     })
     // конец кнопка прокрутки сайта наверх
+
 
     // форма, валидация
     const inputName = document.querySelectorAll('input[name="name"]'),
@@ -79,6 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
     showInfoInput(inputPhone, topInfoPhone, regexPhone, validatePhone);
     // конец форма, валидация
 
+
     // скрипт модальное окно открыть/закрыть
     const knowMoreBtns = document.querySelectorAll('[data-knowmore]'),
           callBackBtns = document.querySelectorAll('[data-callback]'),
@@ -108,10 +111,176 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     // конец скрипт модальное окно открыть/закрыть
 
+
     // слайдер 
 
+    let slider = (containerClass, slidesClass, slideClass, paggination, name) => {
+        const slideContainer = document.querySelector(containerClass);
+        const slide = document.querySelector(slidesClass);
+        const pag = document.querySelector(paggination)
+        
+        let slides = document.querySelectorAll(slideClass);
+        let index = 1;
+        let start;
+        let change;
+        
+        const firstClone = slides[0].cloneNode(true);
+        const lastClone = slides[slides.length -1].cloneNode(true);
+        
+        for (let i = 0; i < slides.length; i++){
+            pag.insertAdjacentHTML("afterbegin", `<div class='pag_item pag_${name}'></div>`);
+        }
+        let pagItem = document.querySelectorAll(`.pag_${name}`);
+        pagItem[0].classList.add('pag_active');
+
+        firstClone.id = 'first-clone';
+        lastClone.id = 'last-clone';
+        
+        slide.append(firstClone);
+        slide.prepend(lastClone);
+        
+        const slideWidth = slides[index].clientWidth;
+        
+        slide.style.transform = `translateX(${(-slideWidth) * index}px)`;
+        
+        const getSlides = () => document.querySelectorAll(slideClass);
+        
+        
+        slide.addEventListener('transitionend', () => {
+            slides = getSlides();
+            if (slides[index].id === firstClone.id) {
+                slide.style.transition = 'none';
+                index = 1;
+                slide.style.transform = `translateX(${-(slideWidth) * index}px)`;
+            }
+            
+            if (slides[index].id === lastClone.id) {
+                slide.style.transition = 'none';
+                index = slides.length - 2;
+                slide.style.transform = `translateX(${-(slideWidth) * index}px)`;
+            }
+        });
+        
+        const moveToNextSlide = () => {
+            slides = getSlides();
+            if (index >= slides.length - 1) return;
+            index++;
+            slide.style.transition = '.7s ease-out';
+            slide.style.transform = `translateX(${(-slideWidth) * index }px)`;
+            let pagItem = document.querySelectorAll(`.pag_${name}`);
+            let pagLength = pagItem.length;
+            if (index - 1 == pagLength) {
+                pagItem[0].classList.add('pag_active');
+                pagItem[pagLength - 1].classList.remove('pag_active');
+            } else {
+                pagItem[index - 1].classList.add('pag_active');
+                pagItem[index - 2].classList.remove('pag_active');
+            }
+        };
+        
+        const moveToPreviousSlide = () => {
+            if (index <= 0) return;
+            index--;
+            slide.style.transition = '.7s ease-out';
+            slide.style.transform = `translateX(${(-slideWidth) * index}px)`;
+            let pagItem = document.querySelectorAll(`.pag_${name}`);
+            let pagLength = pagItem.length;
+            if (index - 1 < 0) {
+                pagItem[pagLength - 1].classList.add('pag_active');
+                pagItem[index].classList.remove('pag_active');
+            } else {
+                pagItem[index - 1].classList.add('pag_active');
+                pagItem[index].classList.remove('pag_active');
+            }
+        };
+        
+        
+        slideContainer.addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            start = e.touches[0].clientX;
+        });
+        slideContainer.addEventListener('touchmove', (e) => {
+            e.preventDefault();
+            let touch = e.touches[0];
+            change = start - touch.clientX;
+        });
+        const swipeSlide = () => {
+            if (change > 0) {
+                moveToNextSlide();
+            } else{
+                moveToPreviousSlide();
+            }
+        }
+        slideContainer.addEventListener('touchend', swipeSlide);
+    }
+
+    let sliderCards = () => {
+        const slideContainer = document.querySelector('.cards_container');
+        const slide = document.querySelector('.cards');
+        const pag = document.querySelector('.pag-cards')
+        
+        let slides = document.querySelectorAll('.card');
+        let index = 0;
+        let start;
+        let change;
+        
+        for (let i = 0; i < slides.length; i++){
+            pag.insertAdjacentHTML("afterbegin", `<div class='pag_item pag_card'></div>`);
+        }
+        let pagItem = document.querySelectorAll(`.pag_card`);
+        pagItem[0].classList.add('pag_active');
+        
+        const slideWidth = slides[index].clientWidth;
+        
+        const getSlides = () => document.querySelectorAll('.card');
+        
+        const moveToNextSlide = () => {
+            slides = getSlides();
+            if (index >= slides.length - 1) return;
+            index++;
+            slide.style.transition = '.7s ease-out';
+            slide.style.transform = `translateX(${(-slideWidth + 14) * index }px)`;
+            let pagItem = document.querySelectorAll(`.pag_card`);
+            pagItem[index].classList.add('pag_active');
+            pagItem[index - 1].classList.remove('pag_active');
+        };
+        
+        const moveToPreviousSlide = () => {
+            if (index <= 0) return;
+            index--;
+            slide.style.transition = '.7s ease-out';
+            slide.style.transform = `translateX(${(-slideWidth + 14) * index}px)`;
+            let pagItem = document.querySelectorAll(`.pag_card`);
+            pagItem[index ].classList.add('pag_active');
+            pagItem[index + 1].classList.remove('pag_active');
+        };
+        
+        
+        slideContainer.addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            start = e.touches[0].clientX;
+        });
+        slideContainer.addEventListener('touchmove', (e) => {
+            e.preventDefault();
+            let touch = e.touches[0];
+            change = start - touch.clientX;
+        });
+        const swipeSlide = () => {
+            if (change > 0) {
+                moveToNextSlide();
+            } else{
+                moveToPreviousSlide();
+            }
+        }
+        slideContainer.addEventListener('touchend', swipeSlide);
+    }
+    slider('.container-olga', '.slides-olga', '.slide-olga', '.pag-olga', 'olga');
+    slider('.container-andrey', '.slides-andrey', '.slide-andrey', '.pag-andrey', 'andrey');
+    slider('.container-sasha', '.slides-sasha', '.slide-sasha', '.pag-sasha', 'sasha');
+    sliderCards();
     
     // конец слайдер 
+
 
     // мобильное меню
     const burgerMenu = document.querySelector('.burger_icon'),
@@ -137,6 +306,5 @@ document.addEventListener("DOMContentLoaded", () => {
             open_burger.classList.toggle('none');
         })
     });
-    
     // конец мобильное меню
 });
